de31e123af4317484edeb425ef57825160178b5f
alex <alexa@DESKTOP-9I9656K>
2023-07-26 12:19:30 +0300

    2nd task

--- ImbueSystemsTest/ImbueSystemsTest/Program.cs
+++ ImbueSystemsTest/ImbueSystemsTest/Program.cs
@@ -5,3 +5,7 @@ var start = new DateTime(2023, 6, 23);
 var end = new DateTime(2023, 7, 20);
 var count = GetSaturdaysCount(start, end);
 Console.WriteLine($"1) Saturdays: {count}");
+// 2nd task:
+var array = new int[] { 1, 2, 5, 3, 2, 4, 5, 6, 2, 1, 4, 6, 6, 6, 7, 1, 3, 4 };
+var result = GetArrayDuplicatesInformation(array);
+Console.WriteLine($"2) {result}");
--- ImbueSystemsTest/ImbueSystemsTest/Utils/TaskHelper.cs
+++ ImbueSystemsTest/ImbueSystemsTest/Utils/TaskHelper.cs
@@ -19,4 +19,31 @@ public static class TaskHelper
         }
         return count;
     }
+
+    public static string GetArrayDuplicatesInformation<TEquatable>(TEquatable[] array) where TEquatable : IEquatable<TEquatable>
+    {
+        if (array == null)
+        {
+            throw new ArgumentNullException();
+        }
+        if (array.Length == 0)
+        {
+            throw new InvalidOperationException("Array was empty.");
+        }
+        var helper = new Dictionary<TEquatable, uint>();
+        for (var index = 0; index < array.Length; index++)
+        {
+            var key = array[index];
+            if (!helper.ContainsKey(key))
+            {
+                helper.Add(key, 1);
+            }
+            else
+            {
+                helper[key]++;
+            }
+        }
+        var newLine = Environment.NewLine;
+        return $"Results:{newLine}{string.Join(newLine, helper.Select(x => $"{x.Key}: {x.Value}"))}";
+    }
 }
