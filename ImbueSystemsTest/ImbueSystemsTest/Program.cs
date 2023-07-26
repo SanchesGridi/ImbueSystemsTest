62df6246314fa0fa3d0c3c57913568ad74b5b23e
alex <alexa@DESKTOP-9I9656K>
2023-07-26 12:14:38 +0300

    1st task

---
+++ ImbueSystemsTest/ImbueSystemsTest/Program.cs
@@ -0,0 +1,7 @@
+﻿using static ImbueSystemsTest.Utils.TaskHelper;
+
+// 1st task:
+var start = new DateTime(2023, 6, 23);
+var end = new DateTime(2023, 7, 20);
+var count = GetSaturdaysCount(start, end);
+Console.WriteLine($"1) Saturdays: {count}");
---
+++ ImbueSystemsTest/ImbueSystemsTest/Utils/TaskHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace ImbueSystemsTest.Utils;
+
+public static class TaskHelper
+{
+    public static uint GetSaturdaysCount(DateTime start, DateTime end)
+    {
+        if (end < start)
+        {
+            throw new InvalidOperationException("The start date must be less than the end date.");
+        }
+        var count = 0u;
+        while (start.Date <= end.Date)
+        {
+            if (start.DayOfWeek == DayOfWeek.Saturday)
+            {
+                count++;
+            }
+            start = start.AddDays(1);
+        }
+        return count;
+    }
+}
